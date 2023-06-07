841d6b4c902dd2f0ae7b9aa09436d910435ad138
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-07 06:46:01 +0000

    commit67

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include "mbed.h"
+
 Serial pc(USBTX,USBRX,9600);
+
 int main()
 {
-    while (true) {
-        pc.printf("Hello World!\r\n");
-        wait_ms(500);
+    int i1=7;
+    float i2=7.5;
+    char i3='H';
+    char c;
+    pc.printf("Hello World\r\n");
+    pc.printf("i1=%d\r\n",i1);
+    pc.printf("i2=%f\r\n",i2);
+    pc.printf("i3=%c\r\n",i3);
+    pc.putc(i3);
+    pc.printf("\r\n");
+
+    for (int j=0;j<10;j++){
+        pc.printf("j=%d  enter any key -->",j);
+        c=pc.getc();
+        pc.putc(c);
+        pc.printf("\r\n");
     }
+    pc.printf("finish!!\r\n");
 }
